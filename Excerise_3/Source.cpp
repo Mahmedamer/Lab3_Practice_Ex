40d6f81ce5ec0fbd0180838110af22d5ae7eaf24
Mahmed amer <67114727+Mahmedamer@users.noreply.github.com>
2020-11-18 02:53:15 +0200

    Ex 3 (Advanced)

--- Excerise_3/Source.cpp
+++ Excerise_3/Source.cpp
@@ -1,6 +1,21 @@
 #include"GameProfile.h"
 #include<iostream>
 using namespace std;
+GameProfile HighestRank(GameProfile Arr[] , int n)
+{
+	GameProfile high = Arr[0];
+	int rank = high.getRank();
+	for (int i = 1; i < n; i++)
+	{
+		int temprank = Arr[i].getRank();
+		if (temprank > rank)
+		{
+			high = Arr[i];
+			rank = temprank;
+		}
+	}
+	return high;
+}
 int main()
 {
 	GameProfile Gamer1("Mohamed" , 6308 , 1000 , 5 , false ,127) , Gamer2;
@@ -46,5 +61,11 @@ int main()
 	cout << "Gamer1 : " << endl;
 	Gamer1.Print();
 
+	GameProfile A[2];
+	A[0] = Gamer1;
+	A[1] = Gamer2;
+	GameProfile G = HighestRank(A, 2);
+	cout << "Gamer G : " << endl;
+	G.Print();
 	return 0;
 }
